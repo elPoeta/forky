2d36dac7e03df09d164ab570db40065b7db31acd
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-05-24 18:52:55 -0300

    add error handler

--- bootstrapApp/router.js
+++ bootstrapApp/router.js
@@ -1,5 +1,6 @@
 const auth = require('../route/api/auth');
-
+const errorHandler = require('../middleware/errorHandler');
 module.exports = app => {
     app.use('/api/v1/auth', auth)
+    app.use(errorHandler);
 }
