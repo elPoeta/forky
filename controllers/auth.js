a746cbb5884dfecdb2000f45fe54c1a0e7b1114f
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-05-24 18:55:53 -0300

    add asybc handler function

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,9 @@
-exports.login = (req, res, next) => {
+const async = require('../middleware/async');
+
+exports.login = async((req, res, next) => {
     res.status(200).json({
         succes: true,
         status: 200,
         data: { test: "ok" }
     })
-}
+});
