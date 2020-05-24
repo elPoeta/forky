2461c5ba9edd1d31884e7327c6c63ec8d60003c0
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-05-24 16:30:28 -0300

    add cors module config parser

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const { PORT } = require('./config/key');
+
+require('./bootstrapApp/parser')(app);
+
 app.listen(PORT, error => {
   if (error) {
+    console.log(`Error to start server ${error}`);
     process.exit(1);
   }
   console.log(`Server running on port ${PORT}`);
