c938d22dd884d140ee895ccee74db697da8a01aa
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-05-27 19:53:35 -0300

    add isEmpty util function

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,25 @@
 const async = require('../middleware/async');
+const isEmpty = require('../utils/isEmpty');
+
+exports.register = async((req, res, next) => {
+    const { name, alias, email, role, password } = req.body;
+
+    res.status(200).json({
+        succes: true,
+        status: 200,
+        data: { test: "ok" }
+    })
+});
 
 exports.login = async((req, res, next) => {
+    res.status(200).json({
+        succes: true,
+        status: 200,
+        data: { test: "ok" }
+    })
+});
+
+exports.logout = async((req, res, next) => {
     res.status(200).json({
         succes: true,
         status: 200,
--- route/api/auth.js
+++ route/api/auth.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router();
-const { login } = require('../../controllers/auth');
+const { register, login, logout } = require('../../controllers/auth');
 
-router.get('/login', login);
+router.post('/register', register);
+router.post('/login', login);
+router.get('/logout', logout);
 
 module.exports = router;
