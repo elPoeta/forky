9d6ae453bd4e93f68d2db5e3d8dbfaf7e1c367aa
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-05-27 21:19:32 -0300

    add login

--- controllers/auth.js
+++ controllers/auth.js
@@ -23,11 +23,24 @@ exports.register = async(async (req, res, next) => {
     })
 });
 
-exports.login = async((req, res, next) => {
+exports.login = async(async (req, res, next) => {
+    const { email, password } = req.body;
+    if (isEmpty(email) || isEmpty(password)) {
+        return next(new ErrorResponse('Please provide an email and password', 400));
+    }
+    const user = await User.findOne({ email }).select('+password');
+    if (!user) {
+        return next(new ErrorResponse("Invalid email", 400));
+    }
+    console.log('user', user)
+    if (password != user.password) {
+        return next(new ErrorResponse("Invalid password", 401));
+    }
+    user.password = undefined;
     res.status(200).json({
         succes: true,
         status: 200,
-        data: { test: "ok" }
+        data: user
     })
 });
 
