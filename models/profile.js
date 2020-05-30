7da0308fb3c952296bb8574aa2012e4605157c20
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-05-30 17:58:36 -0300

    add profile schema

--- models/profile.js
+++ models/profile.js
@@ -1,10 +1,51 @@
+const mongoose = require('mongoose');
 
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  alias: {
+    type: String,
+    unique: true,
+    minlength: 3,
+    maxlength: 25,
+    lowercase: true,
+    required: [true, 'Please add a nick name']
+  },
+  photo: {
+    type: String,
+    default: 'no-photo.jpg'
+  },
+  bio: {
+    type: String
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  social: {
+    linkedin: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    youtube: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
 
-/*alias: {
-  type: String,
-  unique: true,
-  minlength: 3,
-  maxlength: 25,
-  lowercase: true,
-  required: [true, 'Please add a nick name']
-},*/
+module.exports = mongoose.model("Profile", ProfileSchema);
