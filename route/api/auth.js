354966a4528f79cf0f2e287378c3443c01cdb9f4
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-05-31 16:35:19 -0300

    add protect route on logout

--- route/api/auth.js
+++ route/api/auth.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router();
 const { register, login, logout } = require('../../controllers/auth');
+const { authorization } = require('../../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/logout', logout);
+router.get('/logout', authorization, logout);
 
 module.exports = router;
