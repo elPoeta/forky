97b29496fd4554cf1eb095ac62318626d0da2b6f
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-05-31 16:16:32 -0300

    clear cookie on logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,6 +37,10 @@ exports.login = async(async (req, res, next) => {
 });
 
 exports.logout = async((req, res, next) => {
+    res.cookie('token', 'none', {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true
+    });
     res.status(200).json({
         success: true,
         data: {}
